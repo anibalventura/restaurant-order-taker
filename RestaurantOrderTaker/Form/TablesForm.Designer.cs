9ffd52d694b9b150572c9525a47e1fb0269956dc
Anibal Ventura <anibalventura20@gmail.com>
2021-11-04 11:32:05 -0400

    Add TablesForm layout.

---
+++ RestaurantOrderTaker/Form/TablesForm.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace RestaurantOrderTaker
+{
+    partial class TablesForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.BtnTableOne = new System.Windows.Forms.Button();
+            this.BtnTableThree = new System.Windows.Forms.Button();
+            this.BtnTableFour = new System.Windows.Forms.Button();
+            this.BtnTableTwo = new System.Windows.Forms.Button();
+            this.BtnTableFive = new System.Windows.Forms.Button();
+            this.BtnViewOrders = new System.Windows.Forms.Button();
+            this.LblWindowTitle = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.16684F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.55915F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.55915F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 23.55915F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.15571F));
+            this.tableLayoutPanel1.Controls.Add(this.BtnTableOne, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.BtnTableThree, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.BtnTableFour, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.BtnTableTwo, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.BtnTableFive, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.BtnViewOrders, 2, 4);
+            this.tableLayoutPanel1.Controls.Add(this.LblWindowTitle, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(565, 424);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // BtnTableOne
+            // 
+            this.BtnTableOne.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnTableOne.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnTableOne.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnTableOne.Location = new System.Drawing.Point(88, 87);
+            this.BtnTableOne.Name = "BtnTableOne";
+            this.BtnTableOne.Size = new System.Drawing.Size(127, 78);
+            this.BtnTableOne.TabIndex = 0;
+            this.BtnTableOne.Text = "Table # 1";
+            this.BtnTableOne.UseVisualStyleBackColor = false;
+            // 
+            // BtnTableThree
+            // 
+            this.BtnTableThree.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnTableThree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnTableThree.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnTableThree.Location = new System.Drawing.Point(221, 171);
+            this.BtnTableThree.Name = "BtnTableThree";
+            this.BtnTableThree.Size = new System.Drawing.Size(127, 78);
+            this.BtnTableThree.TabIndex = 1;
+            this.BtnTableThree.Text = "Table # 3";
+            this.BtnTableThree.UseVisualStyleBackColor = false;
+            // 
+            // BtnTableFour
+            // 
+            this.BtnTableFour.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnTableFour.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnTableFour.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnTableFour.Location = new System.Drawing.Point(88, 255);
+            this.BtnTableFour.Name = "BtnTableFour";
+            this.BtnTableFour.Size = new System.Drawing.Size(127, 78);
+            this.BtnTableFour.TabIndex = 2;
+            this.BtnTableFour.Text = "Table # 4";
+            this.BtnTableFour.UseVisualStyleBackColor = false;
+            // 
+            // BtnTableTwo
+            // 
+            this.BtnTableTwo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnTableTwo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnTableTwo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnTableTwo.Location = new System.Drawing.Point(354, 87);
+            this.BtnTableTwo.Name = "BtnTableTwo";
+            this.BtnTableTwo.Size = new System.Drawing.Size(127, 78);
+            this.BtnTableTwo.TabIndex = 3;
+            this.BtnTableTwo.Text = "Table # 2";
+            this.BtnTableTwo.UseVisualStyleBackColor = false;
+            // 
+            // BtnTableFive
+            // 
+            this.BtnTableFive.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnTableFive.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BtnTableFive.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnTableFive.Location = new System.Drawing.Point(354, 255);
+            this.BtnTableFive.Name = "BtnTableFive";
+            this.BtnTableFive.Size = new System.Drawing.Size(127, 78);
+            this.BtnTableFive.TabIndex = 4;
+            this.BtnTableFive.Text = "Table # 5";
+            this.BtnTableFive.UseVisualStyleBackColor = false;
+            // 
+            // BtnViewOrders
+            // 
+            this.BtnViewOrders.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnViewOrders.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.BtnViewOrders.Location = new System.Drawing.Point(221, 368);
+            this.BtnViewOrders.Name = "BtnViewOrders";
+            this.BtnViewOrders.Size = new System.Drawing.Size(127, 23);
+            this.BtnViewOrders.TabIndex = 5;
+            this.BtnViewOrders.Text = "View all orders";
+            this.BtnViewOrders.UseVisualStyleBackColor = true;
+            // 
+            // LblWindowTitle
+            // 
+            this.LblWindowTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.LblWindowTitle.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.LblWindowTitle, 3);
+            this.LblWindowTitle.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblWindowTitle.Location = new System.Drawing.Point(88, 27);
+            this.LblWindowTitle.Name = "LblWindowTitle";
+            this.LblWindowTitle.Size = new System.Drawing.Size(393, 30);
+            this.LblWindowTitle.TabIndex = 6;
+            this.LblWindowTitle.Text = "Select a table to take a order";
+            this.LblWindowTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TablesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 424);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "TablesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tables";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button BtnTableOne;
+        private System.Windows.Forms.Button BtnTableThree;
+        private System.Windows.Forms.Button BtnTableFour;
+        private System.Windows.Forms.Button BtnTableTwo;
+        private System.Windows.Forms.Button BtnTableFive;
+        private System.Windows.Forms.Button BtnViewOrders;
+        private System.Windows.Forms.Label LblWindowTitle;
+    }
+}
+
--- RestaurantOrderTaker/Form/TablesForm.cs
+++ RestaurantOrderTaker/Form/TablesForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RestaurantOrderTaker
 {
-    public partial class Form1 : Form
+    public partial class TablesForm : Form
     {
-        public Form1()
+        public TablesForm()
         {
             InitializeComponent();
         }
